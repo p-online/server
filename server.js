90a38869325f1d582a241195d50654bc2a10683f
Jonathan Matarazzi <jonathanmatarazzi@gmail.com>
2017-06-12 17:30:02 +0200

    Declared express
    Declared the express variable to use the express.static function to server the website.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var port = 80;
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app)
 var io = require('socket.io')(server);
 
